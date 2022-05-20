eee4bfee429ccac94907278c81aad733ce8ba94c
kidnaps <56860931+kidnaps@users.noreply.github.com>
2022-05-20 19:42:41 +0300

    lab3 routes.py file

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,35 @@
 from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
- @app.route('/')
- @app.route('/index')
- def hello_world():
-     return 'Hello, World!'
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'kidnaps'}
+
+    posts = [
+        {
+            'author': {'username': 'dave388'},
+            'body': 'my first post ;)'
+        },
+        {
+            'author': {'username': 'annyM'},
+            'body': 'Is this livejournal resurraction?'
+        },
+        {
+            'author': {'username': 'kingJames23'},
+            'body': 'fishing in 2022'
+        }
+    ]
+
+    return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
